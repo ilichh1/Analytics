b900b98ab7467b845682979f2ee1af28694d9284
Ilich Arredondo <ilichh1@hotmail.com>
2017-11-08 14:01:29 -0600

    jQuery y analytic necesarios para hacer el registro de eventos y las peticiones AJAX

--- src/java/servlet/RegistrarClick.java
+++ src/java/servlet/RegistrarClick.java
@@ -5,11 +5,13 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
 /**
  *
@@ -30,5 +32,24 @@ public class RegistrarClick extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        //Iteramos por todos los parametros recibidos y los imprimimos en pantalla
+        
+        Enumeration<String> keys = request.getParameterNames();
+        
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            System.out.println(request.getParameter(key));
+        }
+        
+        //Objeto "out" para mandar datos a la respuesta
+        PrintWriter out = response.getWriter();
+        //Objeto JSON que contendrá la respuesta del serivdor
+        JSONObject json = new JSONObject();
+        json.put("recibido", true);
+        
+        out.print(json.toString());
+        
+        //Matamos este Servlet
+        this.destroy();
     }
 }
