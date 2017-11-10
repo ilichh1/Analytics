286d18c52016ac3d33064f7d7bd5f3fc977ec9a7
Ilich Arredondo <ilichh1@hotmail.com>
2017-11-10 12:01:20 -0600

    Genera un PDF en blanco y lo sirve al cliente

---
+++ src/java/servlet/GeneradorReporte.java
@@ -0,0 +1,100 @@
+/*
+ * Licencia pendiente, para cambiar la licencia ir a -> Tools | Templates
+ */
+package servlet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+/**
+ *
+ * @author Ilich Arredondo
+ */
+@WebServlet(name = "GeneradorReporte", urlPatterns = {"/reporte"})
+public class GeneradorReporte extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        //Creamos un nuevo documento PDF
+        PDDocument pdf = new PDDocument();
+        pdf.addPage(new PDPage());
+        File pdfFile = new File("reporte.pdf");
+        pdf.save(pdfFile);
+
+        //Le decimos al navegador que va a recibir un pdf
+        response.setContentType("application/pdf");
+        response.setContentLength((int) pdfFile.length());
+
+        FileInputStream fileIS = new FileInputStream(pdfFile);
+        OutputStream responseOS = response.getOutputStream();
+
+        int bytes;
+        while ((bytes = fileIS.read()) != -1) {
+            responseOS.write(bytes);
+        }
+        
+        responseOS.close();
+        this.destroy();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
